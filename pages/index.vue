b49111ac2cf1810c910e6b9f3f2d0e0cd43a50c7
HIROAKI IHA <hrak.iha+awesomeHiro@gmail.com>
2020-03-06 21:26:55 +0900

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,8 @@
 <template>
   <div class="container">
     <template v-for="x in board.length">
-      <div
-        v-for="y in board[x - 1].length"
-        :key="`${y}-${x}`"
-        class="cell"
-        @click="putStone(x, y)"
-      >
-        <div
-          v-if="isStone(x, y)"
-          :class="['stone', isBlack(x, y) ? 'black' : 'white']"
-        />
+      <div v-for="y in board[x - 1].length" :key="`${y}-${x}`" class="cell" @click="putStone(x, y)">
+        <div v-if="isStone(x, y)" :class="['stone', isBlack(x, y) ? 'black' : 'white']" />
       </div>
     </template>
   </div>
@@ -18,35 +10,9 @@
 
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator'
-
 @Component
 export default class extends Vue {
   currentColor = 1 // 1: black, 2:white
-
-  get isStone() {
-    return (x: number, y: number) => this.board[x - 1][y - 1] !== 0
-  }
-
-  get isBlack() {
-    return (x: number, y: number) => this.board[x - 1][y - 1] === 1
-  }
-
-  get putStone() {
-    return (x: number, y: number) => {
-      if (this.isStone(x, y)) {
-        return false
-      }
-      this.board[x - 1][y - 1] = this.currentColor
-      this.reRenderBoard()
-      this.currentColor = 3 - this.currentColor
-    }
-  }
-
-  get reRenderBoard() {
-    return () => {
-      this.board = JSON.parse(JSON.stringify(this.board))
-    }
-  }
 
   board = [
     [0, 0, 0, 0, 0, 0, 0, 0],
@@ -58,6 +24,107 @@ export default class extends Vue {
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0]
   ]
+
+  marginBoard = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 2, 0, 0, 0, 0],
+    [0, 0, 0, 0, 2, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+  ]
+
+  connectedBoard = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 1, 1, 1, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 1, 1, 1, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+  ]
+
+  get isStone() {
+    return (x: number, y: number) => this.board[x - 1][y - 1] !== 0
+  }
+
+  get isBlack() {
+    return (x: number, y: number) => this.board[x - 1][y - 1] === 1
+  }
+
+  get setStone() {
+    return (x: number, y: number) => {
+      return (this.board[x - 1][y - 1] = this.currentColor)
+    }
+  }
+
+  get isConnectedWithStone() {
+    return (x: number, y: number) => {
+      return Boolean(this.connectedBoard[x + 1][y + 1])
+    }
+  }
+
+  get passTrun() {
+    return () => {
+      this.currentColor = 3 - this.currentColor
+    }
+  }
+  // TODO: we should use mutate array method
+  get reRenderBoard() {
+    return () => {
+      this.board = JSON.parse(JSON.stringify(this.board))
+    }
+  }
+
+  get updateMarginBoard() {
+    return () => {
+      this.board.forEach((row, y) => {
+        row.forEach((cell, x) => {
+          if (cell) {
+            this.marginBoard[x + 1][y + 1] = cell
+          }
+        })
+      })
+    }
+  }
+
+  get updateConnectedBoard() {
+    return () => {
+      this.board.forEach((row, y) => {
+        row.forEach((cell, x) => {
+          if (cell) {
+            this.connectedBoard[x + 1][y + 1] = 1
+            this.connectedBoard[x + 1][y + 0] = 1
+            this.connectedBoard[x + 1][y - 1] = 1
+            this.connectedBoard[x + 0][y + 1] = 1
+            this.connectedBoard[x + 0][y - 1] = 1
+            this.connectedBoard[x - 1][y + 1] = 1
+            this.connectedBoard[x - 1][y + 0] = 1
+            this.connectedBoard[x - 1][y - 1] = 1
+          }
+        })
+      })
+    }
+  }
+
+  get putStone() {
+    return (x: number, y: number) => {
+      if (this.isStone(x, y)) return false
+      if (this.isConnectedWithStone(x, y)) return false
+      this.setStone(x, y)
+      this.passTrun()
+      this.reRenderBoard()
+      this.updateMarginBoard()
+      this.updateConnectedBoard()
+    }
+  }
 }
 </script>
 
